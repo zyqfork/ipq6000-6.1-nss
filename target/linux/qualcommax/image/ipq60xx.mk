8a5a9d0bf75b3c2f39f61fd5d8a3004da0a14767
pexcn <pexcn97@gmail.com>
2023-12-11 22:17:22 +0800

    qualcommax: add Redmi AX5 support

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -39,6 +39,19 @@ define Device/glinet_gl-axt1800
 endef
 TARGET_DEVICES += glinet_gl-axt1800
 
+define Device/redmi_ax5
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX5
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	SOC := ipq6018
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax5
+endef
+TARGET_DEVICES += redmi_ax5
+
 define Device/xiaomi_ax1800
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
